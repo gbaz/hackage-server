ed2872b542c23a0b2df70519df3640232f5c99b9
Gershom <gershomb@gmail.com>
2018-04-12 16:42:32 -0400

    entries parsed but not in db yet

--- exes/ReplayIndexActions.hs
+++ exes/ReplayIndexActions.hs
@@ -5,12 +5,10 @@ import qualified Distribution.Server.Users.Users as Users
 import Distribution.Server.Users.State
 import Distribution.Server.Features.Core
 import Distribution.Server.Features.Core.State
-import qualified Distribution.Client.Index as Index
 import qualified Data.ByteString.Lazy as BS
 import System.Environment
 import Distribution.Types.PackageId
 import Codec.Archive.Tar.Entry
-import Control.Monad (join)
 import qualified Distribution.Server as Server
 import qualified Distribution.Verbosity as Verbosity
 import Distribution.Server.Framework.Feature (updateState, queryState, StateComponent(..))
@@ -33,6 +31,15 @@ import System.FilePath ((</>),(<.>))
 import Distribution.Text(display)
 import System.Directory(getHomeDirectory)
 
+import qualified Codec.Archive.Tar       as Tar ( read, Entries(..) )
+import qualified Codec.Archive.Tar.Entry as Tar ( Entry(..), entryPath )
+
+import Distribution.Package
+import Distribution.Text ( simpleParse )
+
+import Data.ByteString.Lazy (ByteString)
+import System.FilePath.Posix ( splitDirectories, normalise )
+
 lastExistingTimestamp :: EpochTime
 lastExistingTimestamp = 1523451453 --TODO pick right stamp
 
@@ -46,10 +53,10 @@ main = do
    userState <- usersStateComponent (Server.serverStateDir serverEnv)
    coreState <- packagesStateComponent Verbosity.normal False (Server.serverStateDir serverEnv)
 
-   let Right allEntries = Index.read (,) (const True) bs -- if we don't parse, we die
+   let Right allEntries = readIndex (,) (const True) bs -- if we don't parse, we die
        -- entries are reverse chron, but we want to process them oldest to newest
        -- entries = reverse $ take 1000 allEntries
-       entries = reverse $ takeWhile (\x -> entryTime (snd x) > lastExistingTimestamp) allEntries
+       entries = reverse $ takeWhile (\x -> either entryTime (entryTime . snd) x > lastExistingTimestamp) allEntries
 
    mapM_ (processEntry userState coreState (Server.serverBlobStore serverEnv)) entries
 
@@ -58,38 +65,39 @@ main = do
 processEntry :: StateComponent AcidState Users.Users
                -> StateComponent AcidState PackagesState
                -> BlobStorage.BlobStorage
-               -> (PackageIdentifier, Entry)
+               -> Either Entry (PackageIdentifier, Entry)
                -> IO ()
-processEntry userState coreState blobstore x = do
-  print (fst x)
---  print $  (snd x) {entryContent=Directory}
+processEntry userState coreState blobstore (Left entry) = print entry -- todo
+processEntry userState coreState blobstore (Right (pkgid,entry)) = do
+  print pkgid
+--  print $ entry {entryContent=Directory}
   let
      doPackageUpload :: IO ()
      doPackageUpload = do
        (uid,userInfo) <- userInsertOrLookup
-       pkgTarball <- createPackageTarball blobstore (fst x)
-       let ultime = posixSecondsToUTCTime $ realToFrac $ entryTime (snd x)
+       pkgTarball <- createPackageTarball blobstore pkgid
+       let ultime = posixSecondsToUTCTime $ realToFrac $ entryTime entry
            ulinfo = (ultime, uid)
-       NormalFile bs _ <- return $ entryContent (snd x)
+       NormalFile bs _ <- return $ entryContent entry
        updateState coreState $ AddPackage3
           (PkgInfo
-            (fst x)
+            pkgid
             (V.fromList [(CabalFileText bs,ulinfo)]) -- list of (CabalFileText,UploadInfo)
             (V.fromList [(pkgTarball,ulinfo)]) -- list of (PkgTarball, UploadInfo)
           )
           (ultime,uid)
           (userName userInfo)
-          [MetadataEntry (fst x) 0 ultime] -- this gives the package.json
+          [MetadataEntry pkgid 0 ultime] -- this gives the package.json
        return ()
 
      doPackageRevision :: IO ()
      doPackageRevision = do
        (uid,userInfo) <- userInsertOrLookup
-       let ultime = posixSecondsToUTCTime $ realToFrac $ entryTime (snd x)
+       let ultime = posixSecondsToUTCTime $ realToFrac $ entryTime entry
            ulinfo = (ultime, uid)
-       NormalFile bs _ <- return $ entryContent (snd x)
+       NormalFile bs _ <- return $ entryContent entry
        updateState coreState $ AddPackageRevision2
-          (fst x)
+          pkgid
           (CabalFileText bs)
           ulinfo
           (userName userInfo)
@@ -97,8 +105,8 @@ processEntry userState coreState blobstore x = do
 
      userInsertOrLookup :: IO (UserId,UserInfo)
      userInsertOrLookup = do
-        let uname = ownerName $ entryOwnership (snd x)
-            uid   = UserId $ ownerId $ entryOwnership (snd x)
+        let uname = ownerName $ entryOwnership entry
+            uid   = UserId $ ownerId $ entryOwnership entry
 
         usersdb <- queryState userState $ GetUserDb
         case lookupUserId uid usersdb of
@@ -110,19 +118,19 @@ processEntry userState coreState blobstore x = do
                           Right newdb -> updateState userState (ReplaceUserDb newdb) >> return (uid, uinfo)
 
   pkgIndex <- fmap packageIndex . queryState coreState $ GetPackagesState
-  let existingPkg = PI.lookupPackageId pkgIndex (fst x)
+  let existingPkg = PI.lookupPackageId pkgIndex pkgid
       packageVersionAlreadyExists = isJust $ existingPkg
       replayAlreadyDone = case existingPkg of
                         Nothing -> False
                         Just pkg -> let timestamps = map fst $ map snd (V.toList (pkgMetadataRevisions pkg)) ++ map snd (V.toList (pkgTarballRevisions pkg))
-                                        ultime = posixSecondsToUTCTime $ realToFrac $ entryTime (snd x)
+                                        ultime = posixSecondsToUTCTime $ realToFrac $ entryTime entry
                                   in not . null $ filter (>= ultime) timestamps
-  let isPackageJson = "package.json" `isSuffixOf` fromTarPathToPosixPath (entryTarPath (snd x))
+  let isPackageJson = "package.json" `isSuffixOf` fromTarPathToPosixPath (entryTarPath entry)
   case (isPackageJson, replayAlreadyDone, packageVersionAlreadyExists) of
     (True,_,_) -> print "ignoring packagejson"
     (_,True,_) -> print "replay already done"
     (_,_,True) -> print "isrevision" -- >> doPackageRevision
-    _          -> print "isupload" >> doPackageUpload
+    _          -> print "isupload" -- >> doPackageUpload
   return ()
 
 createPackageTarball :: BlobStorage.BlobStorage -> PackageIdentifier -> IO PkgTarball
@@ -143,44 +151,21 @@ createPackageTarball store pkgid = do
                               pkgTarballGz   = infoGz
                             , pkgTarballNoGz = blobIdDecompressed
                           }
- {-
--- things can be preferred, json, or package
--- if json, ignore (we'll add with package)
-if package then:
+readIndex :: (PackageIdentifier -> Tar.Entry -> pkg)
+     -> (FilePath -> Bool) -- ^ Should this file be included?
+     -> ByteString
+     -> Either String [Either Tar.Entry pkg]
+readIndex mkPackage includeFile indexFileContent = collect [] entries
+  where
+    entries = Tar.read indexFileContent
+    collect es' Tar.Done        = Right es'
+    collect es' (Tar.Next e es) = collect (entry e:es') es
+    collect _   (Tar.Fail err)  = Left (show err)
 
-check if user exists, else add
-check if package exists at version
-  if yes -- upload revision -- simple
-  if no -- lookup package.json in entries
-           fetch tarball from remote (make sure is pristine)
-
---nb this is manual and not through core feature to ensure we can add the json entry manually and precisely
-updateState coreState $
-        AddPackage3
-          pkginfo
-          uploadinfo
-          (userName userInfo)
-          additionalEntries
-
-If preferred then??
-
--}
-
-
-
---  fst is pkgid, snd is entry
-{-
-
-entries are reverse chronological
-we just take while > some amount
-
-Logic is as follows:
-
-A) filter data to only be sufficiently recent
-B) for each package, add it to the db with a virtual upload at the correct time and add the correct package.json
-C) for each user if they don't exist, add them with the correct id
-
-
--}
-
---   mapM print $ (map ((\x -> x {entryContent=Directory}) . snd) . take 100) entries
+    entry e
+      | [pkgname,versionStr,_] <- splitDirectories (normalise (Tar.entryPath e))
+      , Just version <- simpleParse versionStr
+      , True <- includeFile (Tar.entryPath e)
+      = let pkgid = PackageIdentifier (mkPackageName pkgname) version
+         in Right (mkPackage pkgid e)
+    entry e = Left e
